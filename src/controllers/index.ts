3587a61a4ba652173dd4379ae542b230854179ad
Adrián González Expósito <alu0101404813@ull.edu.es>
2024-03-03 23:02:57 +0000

    collection controller created, the API works with basic CRUD operations

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,3 +2,4 @@ export * from './ping.controller';
 export * from './user.controller';
 export * from './product.controller';
 export * from './category.controller';
+export * from './collection.controller';
