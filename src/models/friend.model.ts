5535a385c38241a457b1319474ab6ec19f36c8cc
Adrián González Expósito <alu0101404813@ull.edu.es>
2024-03-05 20:23:06 +0000

    friends model created

---
+++ src/models/friend.model.ts
@@ -0,0 +1,34 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Friend extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  _id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  userId: string;
+
+  @property({
+    type: 'array',
+    itemType: 'string',
+  })
+  friends?: string[];
+
+
+  constructor(data?: Partial<Friend>) {
+    super(data);
+  }
+}
+
+export interface FriendRelations {
+  // describe navigational properties here
+}
+
+export type FriendWithRelations = Friend & FriendRelations;
