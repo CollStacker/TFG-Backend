caa8d6eb0b6032718555b5250be594fb6ccd6d59
Adrián González Expósito <alu0101404813@ull.edu.es>
2024-03-29 08:54:22 +0000

    creating productfield repository

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -3,3 +3,4 @@ export * from './collection.repository';
 export * from './product.repository';
 export * from './user.repository';
 export * from './friend.repository';
+export * from './product-field.repository';
