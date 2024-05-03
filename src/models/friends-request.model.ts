d50228f2ab0ee582b059293c0a0d3a085baed72d
Adrián González Expósito <alu0101404813@ull.edu.es>
2024-05-03 09:28:23 +0100

    creating new model for friends requests lsit and his correspondent repository

---
+++ src/models/friends-request.model.ts
@@ -0,0 +1,34 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class FriendsRequest extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  _id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  userId: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  friendId: string;
+
+
+  constructor(data?: Partial<FriendsRequest>) {
+    super(data);
+  }
+}
+
+export interface FriendsRequestRelations {
+  // describe navigational properties here
+}
+
+export type FriendsRequestWithRelations = FriendsRequest & FriendsRequestRelations;
