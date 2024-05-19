e76fa59337add31ec6b459953f83e1cbc212cf02
Adrián González Expósito <alu0101404813@ull.edu.es>
2024-05-19 19:55:42 +0100

    created new endpoint to get las 20 products inserted in the db

---
+++ src/interfaces/homeViewProductData.interface.ts
@@ -0,0 +1,8 @@
+export interface HomeViewProductDataInterface {
+  _id: string, // productId
+  name: string,
+  description: string,
+  image?: string,
+  publicationDate?: Date,
+  ownerId: string,
+}
