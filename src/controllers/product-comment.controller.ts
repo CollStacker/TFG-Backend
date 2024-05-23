214ff3ceff86629ac43ebf5ecd6354ba0cfb2562
Adrián González Expósito <alu0101404813@ull.edu.es>
2024-05-23 15:57:54 +0100

    commenting deprecated endpoints and creating new one to find all comments of determinated product

--- src/controllers/product-comment.controller.ts
+++ src/controllers/product-comment.controller.ts
@@ -1,29 +1,31 @@
 import {
-  Count,
-  CountSchema,
-  Filter,
-  FilterExcludingWhere,
+  // Count,
+  // CountSchema,
+  // Filter,
+  // FilterExcludingWhere,
   repository,
-  Where,
+  // Where,
 } from '@loopback/repository';
 import {
   post,
   param,
   get,
   getModelSchemaRef,
-  patch,
-  put,
+  // patch,
+  // put,
   del,
   requestBody,
   response,
 } from '@loopback/rest';
 import {ProductComments} from '../models';
-import {ProductCommentsRepository} from '../repositories';
+import {ProductCommentsRepository, ProductRepository} from '../repositories';
 
 export class ProductCommentController {
   constructor(
     @repository(ProductCommentsRepository)
     public productCommentsRepository : ProductCommentsRepository,
+    @repository(ProductRepository)
+    public productRepository : ProductRepository,
   ) {}
 
   @post('/productComments')
@@ -47,55 +49,7 @@ export class ProductCommentController {
     return this.productCommentsRepository.create(productComments);
   }
 
-  @get('/productComments/count')
-  @response(200, {
-    description: 'ProductComments model count',
-    content: {'application/json': {schema: CountSchema}},
-  })
-  async count(
-    @param.where(ProductComments) where?: Where<ProductComments>,
-  ): Promise<Count> {
-    return this.productCommentsRepository.count(where);
-  }
-
-  @get('/productComments')
-  @response(200, {
-    description: 'Array of ProductComments model instances',
-    content: {
-      'application/json': {
-        schema: {
-          type: 'array',
-          items: getModelSchemaRef(ProductComments, {includeRelations: true}),
-        },
-      },
-    },
-  })
-  async find(
-    @param.filter(ProductComments) filter?: Filter<ProductComments>,
-  ): Promise<ProductComments[]> {
-    return this.productCommentsRepository.find(filter);
-  }
-
-  @patch('/productComments')
-  @response(200, {
-    description: 'ProductComments PATCH success count',
-    content: {'application/json': {schema: CountSchema}},
-  })
-  async updateAll(
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(ProductComments, {partial: true}),
-        },
-      },
-    })
-    productComments: ProductComments,
-    @param.where(ProductComments) where?: Where<ProductComments>,
-  ): Promise<Count> {
-    return this.productCommentsRepository.updateAll(productComments, where);
-  }
-
-  @get('/productComments/{id}')
+  @get('/productComments/byProductId/{productId}')
   @response(200, {
     description: 'ProductComments model instance',
     content: {
@@ -105,39 +59,9 @@ export class ProductCommentController {
     },
   })
   async findById(
-    @param.path.string('id') id: string,
-    @param.filter(ProductComments, {exclude: 'where'}) filter?: FilterExcludingWhere<ProductComments>
-  ): Promise<ProductComments> {
-    return this.productCommentsRepository.findById(id, filter);
-  }
-
-  @patch('/productComments/{id}')
-  @response(204, {
-    description: 'ProductComments PATCH success',
-  })
-  async updateById(
-    @param.path.string('id') id: string,
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(ProductComments, {partial: true}),
-        },
-      },
-    })
-    productComments: ProductComments,
-  ): Promise<void> {
-    await this.productCommentsRepository.updateById(id, productComments);
-  }
-
-  @put('/productComments/{id}')
-  @response(204, {
-    description: 'ProductComments PUT success',
-  })
-  async replaceById(
-    @param.path.string('id') id: string,
-    @requestBody() productComments: ProductComments,
-  ): Promise<void> {
-    await this.productCommentsRepository.replaceById(id, productComments);
+    @param.path.string('productId') productId: string,
+  ): Promise<ProductComments[] | null> {
+    return this.productCommentsRepository.find({where: {productId: productId}})
   }
 
   @del('/productComments/{id}')
@@ -147,4 +71,97 @@ export class ProductCommentController {
   async deleteById(@param.path.string('id') id: string): Promise<void> {
     await this.productCommentsRepository.deleteById(id);
   }
+
+  // @get('/productComments/count')
+  // @response(200, {
+  //   description: 'ProductComments model count',
+  //   content: {'application/json': {schema: CountSchema}},
+  // })
+  // async count(
+  //   @param.where(ProductComments) where?: Where<ProductComments>,
+  // ): Promise<Count> {
+  //   return this.productCommentsRepository.count(where);
+  // }
+
+  // @get('/productComments')
+  // @response(200, {
+  //   description: 'Array of ProductComments model instances',
+  //   content: {
+  //     'application/json': {
+  //       schema: {
+  //         type: 'array',
+  //         items: getModelSchemaRef(ProductComments, {includeRelations: true}),
+  //       },
+  //     },
+  //   },
+  // })
+  // async find(
+  //   @param.filter(ProductComments) filter?: Filter<ProductComments>,
+  // ): Promise<ProductComments[]> {
+  //   return this.productCommentsRepository.find(filter);
+  // }
+
+  // @patch('/productComments')
+  // @response(200, {
+  //   description: 'ProductComments PATCH success count',
+  //   content: {'application/json': {schema: CountSchema}},
+  // })
+  // async updateAll(
+  //   @requestBody({
+  //     content: {
+  //       'application/json': {
+  //         schema: getModelSchemaRef(ProductComments, {partial: true}),
+  //       },
+  //     },
+  //   })
+  //   productComments: ProductComments,
+  //   @param.where(ProductComments) where?: Where<ProductComments>,
+  // ): Promise<Count> {
+  //   return this.productCommentsRepository.updateAll(productComments, where);
+  // }
+
+  // @get('/productComments/{id}')
+  // @response(200, {
+  //   description: 'ProductComments model instance',
+  //   content: {
+  //     'application/json': {
+  //       schema: getModelSchemaRef(ProductComments, {includeRelations: true}),
+  //     },
+  //   },
+  // })
+  // async findById(
+  //   @param.path.string('id') id: string,
+  //   @param.filter(ProductComments, {exclude: 'where'}) filter?: FilterExcludingWhere<ProductComments>
+  // ): Promise<ProductComments> {
+  //   return this.productCommentsRepository.findById(id, filter);
+  // }
+
+  // @patch('/productComments/{id}')
+  // @response(204, {
+  //   description: 'ProductComments PATCH success',
+  // })
+  // async updateById(
+  //   @param.path.string('id') id: string,
+  //   @requestBody({
+  //     content: {
+  //       'application/json': {
+  //         schema: getModelSchemaRef(ProductComments, {partial: true}),
+  //       },
+  //     },
+  //   })
+  //   productComments: ProductComments,
+  // ): Promise<void> {
+  //   await this.productCommentsRepository.updateById(id, productComments);
+  // }
+
+  // @put('/productComments/{id}')
+  // @response(204, {
+  //   description: 'ProductComments PUT success',
+  // })
+  // async replaceById(
+  //   @param.path.string('id') id: string,
+  //   @requestBody() productComments: ProductComments,
+  // ): Promise<void> {
+  //   await this.productCommentsRepository.replaceById(id, productComments);
+  // }
 }
